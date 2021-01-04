1db531448cea51c80bf050ada54c2a84a3bffef0
claudioperez <50180406+claudioperez@users.noreply.github.com>
2021-01-04 12:15:11 -0800

    Cleaning `sphinx` module.

--- src/rendre/sphinx/__init__.py
+++ src/rendre/sphinx/__init__.py
@@ -45,7 +45,6 @@ class SphinxRendre(TocTree):
     def run_link(self,base_args,arg_pairs,fields):
         template = fields.replace("--link","").strip()
         field_pointers = [f for f in template.split("/") if "%" in f]
-        # print(f"FIELD Points: {field_pointers}\n\n")
         filt_args = [x for pair in arg_pairs for x in pair 
             if pair[0] in ["include-item","exclusive-include"] ]
         filt_args = ["--" + x if (i+1)%2 else x.strip() for i, x in enumerate(filt_args)]
@@ -60,10 +59,8 @@ class SphinxRendre(TocTree):
         return res
 
 
-
     def run(self):
         config = self.state.document.settings.env.config.rendre_config
-        print(f"URI: {self.state.document.settings.env.app.builder}")
         config.update({
             "base_uri":
                 self.state.document.settings.env.app.srcdir,
@@ -79,26 +76,27 @@ class SphinxRendre(TocTree):
         ])
         arg_pairs = [self.arg_pat.match(arg).groups() for arg in self.content]
         cmd_args = self.format_arg_options(self.proc_args(arg_pairs))
-        # print(f"URI:\n{directives.uri('qfem-0001/qfem-0001.png')}")
 
         # create HTML output
-        # parsed_args = parser.parse_args([*base_args, cmd, *cmd_args])
         parsed_args = parser.parse_args([*base_args, "-vv", cmd, "--html", *cmd_args])
         html_attributes = {"format": "html"}
         try:
+        #     "a"+0
+        # except:
             html:str = rendre(parsed_args,config=config)
         except Exception as e:
             logger.error(e)
             return [nodes.raw("", e, format="html")]
         else:
+        # if True:
             html_node = nodes.raw("", html, **html_attributes)
             (html_node.source,
             html_node.line) = self.state_machine.get_source_and_line(self.lineno)
 
             # create LaTeX output
-            parsed_args = parser.parse_args([*base_args, cmd, *cmd_args])
+            parsed_args = parser.parse_args([*base_args, cmd, "--latex", *cmd_args])
             latex:str = rendre(parsed_args,config=config)
-            # print(f"LaTeX: \n{latex}")
+            print(f"LaTeX: \n{latex}")
             latex_node = nodes.raw("", latex, format="latex")
             latex_node.source, latex_node.line = html_node.source, html_node.line
 
@@ -107,7 +105,8 @@ class SphinxRendre(TocTree):
                 self.content = self.run_link(base_args,arg_pairs,link)
                 self.options = {"hidden": True, "glob": True}
                 toc = super().run()
-                # print(toc)
             else:
                 toc = []
             return [*toc, latex_node, html_node]
+
+
