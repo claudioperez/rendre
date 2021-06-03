fde6b246cff3e3d5fde80105f15bf2d896f1b2f5
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-06-03 12:40:44 -0500

    cmp - bump aurore dep.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     install_requires=[
         "coloredlogs",
         # 'aurore @ git+https://github.com/claudioperez/aurore@master',
-        "aurore>0.0.7",
+        "aurore>0.0.8",
         "pyyaml",
         "jinja2"
     ]
